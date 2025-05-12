c6b80d8c5df182e4212775d5d768eea72f3a752a
roypsetec <roypstcc@gmail.com>
2025-05-12 20:09:41 -0300

    atualização calendário nomes

--- etask/src/screens/EmBreveScreen.js
+++ etask/src/screens/EmBreveScreen.js
@@ -9,7 +9,8 @@ import { collection, addDoc, getDocs, query, where, deleteDoc, doc, Timestamp, u
 import { db } from '../firebase/firebaseConfig';
 import { getAuth } from 'firebase/auth';
 import { ToastAndroid } from 'react-native';
-import { Calendar } from 'react-native-calendars';
+import { Calendar, LocaleConfig } from 'react-native-calendars';
+
 
 const EmBreveScreen = () => {
   const [showTaskInput, setShowTaskInput] = useState(false);
@@ -165,6 +166,18 @@ const EmBreveScreen = () => {
       );
     }
   };
+
+  LocaleConfig.locales['pt-br'] = {
+  monthNames: ['Janeiro', 'Fevereiro', 'Março', 'Abril', 'Maio', 'Junho', 'Julho', 'Agosto', 'Setembro', 'Outubro', 'Novembro', 'Dezembro'],
+  monthNamesShort: ['Jan', 'Fev', 'Mar', 'Abr', 'Mai', 'Jun', 'Jul', 'Ago', 'Set', 'Out', 'Nov', 'Dez'],
+  dayNames: ['Domingo', 'Segunda-feira', 'Terça-feira', 'Quarta-feira', 'Quinta-feira', 'Sexta-feira', 'Sábado'],
+  dayNamesShort: ['Dom', 'Seg', 'Ter', 'Qua', 'Qui', 'Sex', 'Sáb'],
+  today: 'Hoje'
+};
+
+LocaleConfig.defaultLocale = 'pt-br';
+
+  
 
   return (
     <View style={styles.container}>
